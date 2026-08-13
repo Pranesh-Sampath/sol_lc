20d6d44746b7f54de2af2d9e3d03a8d1539fb1d0
Pranesh-Sampath <mahessampath83@gmail.com>
2026-08-13 11:49:37 +0530

    Time: 1 ms (75.87%), Space: 42.9 MB (42.35%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+     String[]v1=version1.split("\\.");
+      String[]v2=version2.split("\\.");
+      int n=Math.max(v1.length,v2.length);
+      for(int i=0;i<n;i++){
+        int nums1=0;
+        int nums2=0;
+        if(i<v1.length){
+            nums1=Integer.parseInt(v1[i]);
+
+        }
+        if(i<v2.length){
+            nums2=Integer.parseInt(v2[i]);
+        }
+        if(nums1<nums2){
+            return -1;
+        }
+        if(nums1>nums2){
+            return 1;
+        }
+      }
+      return 0;
+    }
+}
+     
+     
+     
+     
+     
+     
+
+     
+
+     
